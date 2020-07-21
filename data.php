e1911b6ab1bc4ed0aa6099d1296c98435b6728f7
Rutuja3806 <rutu.jagtap555@gmail.com>
2020-07-21 18:54:05 +0530

    my first website

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+include 'contact.html';
+include 'connection.php';
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$country = $_POST['country'];
+$subject = $_POST['subject'];
+$sql = "INSERT INTO `contact` (`firstname`,`lastname`,`country`,`subject`)
+ VALUES('$firstname','$lastname','$country','$subject')";
+
+$result = mysqli_query($conn,$sql);
+  
+if ($result== TRUE){
+    echo ( "<script type='text/javascript'>alert('Thank You for Contacting Us..');
+    windows.history.log(-1);
+    
+     </script>");}
+
+
+
+?>
