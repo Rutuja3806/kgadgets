89c226eaf78c2cb25ece1f5415ad29183f82662b
Rutuja3806 <rutu.jagtap555@gmail.com>
2020-07-21 20:13:46 +0530

    c

--- data.php
+++ data.php
@@ -5,8 +5,8 @@ $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $country = $_POST['country'];
 $subject = $_POST['subject'];
-$sql = "INSERT INTO `contact` (`firstname`,`lastname`,`country`,`subject`)
- VALUES('$firstname','$lastname','$country','$subject')";
+
+$sql = "INSERT INTO `contact` (`firstname`,`lastname`,`country`,`subject`) VALUES('$firstname','$lastname','$country','$subject')";
 
 $result = mysqli_query($conn,$sql);
   
